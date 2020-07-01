b7b81663dbc95aa58b2c656888b46bc311d99cf0
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-06-30 18:12:10 -0700

    Setup HTML response (data in JSON for now)

--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -11,9 +11,46 @@ app.use(express.static('../client'));
 
 // for get we go to static, for post we do this:
 app.post('/', (req, res) => {
-  console.log(req.body);
-  var data = req.body;
+  var json = JSON.stringify(req.body);
+  var data = template(json, json);
   res.send(data);
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+
+// html to send back
+var template = function(oldFormat, newFormat) {
+  return `
+<!DOCTYPE html><html>
+<head>
+<title>Json to CSV converter</title>
+<link rel="stylesheet" href="css/main.css">
+<link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+</head>
+<body>
+<div id="main-container">
+<h1>JSON to CSV</h1>
+<div id="form-container">
+<h2>Paste your JSON here:</h2>
+<form id="form" action="/" method="post">
+<p>
+<label for="paste-json"></label>
+<textarea id="paste-json" name="json" rows="20" cols="80">${oldFormat}</textarea>
+</p>
+<input type="submit" id="submit">
+</form>
+</div>
+<div>
+<p id="form-result">
+<h2>Your CSV:</h2>
+<label for="result"></label>
+<textarea id="result" name="result" rows="20" cols="80">${newFormat}</textarea>
+</p>
+</div>
+</div>
+<script src="app.js"></script>
+</body>
+</html>
+`};
