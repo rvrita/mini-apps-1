fb4197368374cae873305f2811084af43a226458
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-07-03 18:18:10 -0700

    Initial setup for React and Express

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,8 @@
 
 
+
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.use(express.static('public'))
 
+app.get('/hi', (req, res) => res.send('Hello World!2'))
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
