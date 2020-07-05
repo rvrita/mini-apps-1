b8e858763a8903831c7fdbc2f02ff17331cb02b7
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-07-04 17:25:21 -0700

    Get form data from client to server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -11,6 +11,7 @@ class Form1 extends React.Component {
 
   // updates the value of the input field in the state
   handleInputChange(e) {
+    e.preventDefault();
     var name = e.target.name;
     this.setState({ [name]: e.target.value });
   }
@@ -19,18 +20,25 @@ class Form1 extends React.Component {
     return (
       <div>
         <p className="text">Please provide name, email and password:</p>
-      <form onSubmit={(e) => this.props.switchForm(this.props.formValue)}>
-        <label>
-          Name: <input type="text" value={this.state.valueName} name="valueName" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Email: <input type="text" value={this.state.valueEmail} name="valueEmail" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Password: <input type="text" value={this.state.valuePassword} name="valuePassword" onChange={this.handleInputChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+        <form onSubmit={(e) => {
+          this.props.switchForm({
+            formValue: this.props.formValue,
+            name: this.state.valueName,
+            email: this.state.valueEmail,
+            password: this.state.valuePassword
+          })
+        }} >
+          <label>
+            Name: <input type="text" value={this.state.valueName} name="valueName" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Email: <input type="text" value={this.state.valueEmail} name="valueEmail" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Password: <input type="text" value={this.state.valuePassword} name="valuePassword" onChange={this.handleInputChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
@@ -53,6 +61,7 @@ class Form2 extends React.Component {
 
   // updates the value of the input field in the state
   handleInputChange(e) {
+    e.preventDefault();
     var name = e.target.name;
     this.setState({ [name]: e.target.value });
   }
@@ -61,27 +70,37 @@ class Form2 extends React.Component {
     return (
       <div>
         <p className="text">Please provide address and phone number:</p>
-      <form onSubmit={(e) => this.props.switchForm(this.props.formValue)}>
-        <label>
-          Address: <input type="text" value={this.state.valueLine1} name="valueLine1" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          <input type="text" value={this.state.valueLine2} name="valueLine2" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          City: <input type="text" value={this.state.valueCity} name="valueCity" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          State: <input type="text" value={this.state.valueState} name="valueState" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Zip code: <input type="text" value={this.state.valueZip} name="valueZip" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Phone number: <input type="text" value={this.state.valuePhone} name="valuePhone" onChange={this.handleInputChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+        <form onSubmit={(e) => {
+          this.props.switchForm({
+            line1: this.state.valueLine1,
+            line2: this.state.valueLine2,
+            city: this.state.valueCity,
+            state: this.state.valueState,
+            zip: this.state.valueZip,
+            phone: this.state.valuePhone,
+            formValue: this.props.formValue
+          })
+        }}>
+          <label>
+            Address: <input type="text" value={this.state.valueLine1} name="valueLine1" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            <input type="text" value={this.state.valueLine2} name="valueLine2" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            City: <input type="text" value={this.state.valueCity} name="valueCity" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            State: <input type="text" value={this.state.valueState} name="valueState" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Zip code: <input type="text" value={this.state.valueZip} name="valueZip" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Phone number: <input type="text" value={this.state.valuePhone} name="valuePhone" onChange={this.handleInputChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
@@ -102,6 +121,7 @@ class Form3 extends React.Component {
 
   // updates the value of the input field in the state
   handleInputChange(e) {
+    e.preventDefault();
     var name = e.target.name;
     this.setState({ [name]: e.target.value });
   }
@@ -110,25 +130,30 @@ class Form3 extends React.Component {
     return (
       <div>
         <p className="text">Please provide credit card information:</p>
-      <form onSubmit={(e) => this.props.switchForm(this.props.formValue)}>
-        <label>
-          Credit Card number: <input type="text" value={this.state.valueCreditCard} name="valueCreditCard" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Expiration Date: <input type="text" value={this.state.valueExpDate} name="valueExpDate" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          CVV: <input type="text" value={this.state.valueCVV} name="valueCVV" onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Billing zip code: <input type="text" value={this.state.valueBillingZip} name="valueBillingZip" onChange={this.handleInputChange} />
-        </label>
-        <input type="submit" value="Submit"/>
-      </form>
+        <form onSubmit={(e) => this.props.switchForm({
+          ccard: this.state.valueCreditCard,
+          expdate: this.state.valueExpDate,
+          cvv: this.state.valueCVV,
+          bzip: this.state.valueBillingZip,
+          formValue: this.props.formValue
+        })}>
+          <label>
+            Credit Card number: <input type="text" value={this.state.valueCreditCard} name="valueCreditCard" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Expiration Date: <input type="text" value={this.state.valueExpDate} name="valueExpDate" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            CVV: <input type="text" value={this.state.valueCVV} name="valueCVV" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Billing zip code: <input type="text" value={this.state.valueBillingZip} name="valueBillingZip" onChange={this.handleInputChange} />
+          </label>
+          <input type="submit" value="Purchase" />
+        </form>
       </div>
     )
   }
-
 };
 
 class CheckoutPage extends React.Component {
@@ -141,7 +166,7 @@ class CheckoutPage extends React.Component {
       <div>
         <h1>Welcome to checkout app!</h1>
         <p className="main-page">Please click on the button.</p>
-        <button onClick={(e) => this.props.switchForm(this.props.formValue)}>
+        <button onClick={(e) => this.props.switchForm({ formValue: this.props.formValue })}>
           Checkout
         </button>
       </div>
@@ -160,21 +185,29 @@ class CheckoutApp extends React.Component {
     this.switchForm = this.switchForm.bind(this);
   }
 
-  switchForm(value) {
+  switchForm(data) {
+    fetch('/checkout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data) })
+      .then(data => console.log(data));
+    // console.log(data);
     this.setState({
-      formValue: value
+      formValue: data.formValue
     })
   }
 
   render() {
     if (this.state.formValue === 0) {
-      return <CheckoutPage formValue={1} switchForm={this.switchForm}/>;
+      return <CheckoutPage formValue={1} switchForm={this.switchForm} />;
     } else if (this.state.formValue === 1) {
-      return <Form1 formValue={2} switchForm={this.switchForm}/>;
+      return <Form1 formValue={2} switchForm={this.switchForm} />;
     } else if (this.state.formValue === 2) {
-      return <Form2 formValue={3} switchForm={this.switchForm}/>;
+      return <Form2 formValue={3} switchForm={this.switchForm} />;
     } else if (this.state.formValue === 3) {
-      return <Form3 formValue={0} switchForm={this.switchForm}/>;
+      return <Form3 formValue={0} switchForm={this.switchForm} />;
     }
   }
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,9 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var crypto = require('crypto');
+var mysql = require('mysql');
 
-app.use(express.static('public'))
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/hi', (req, res) => res.send('Hello World!2'))
+app.use(express.urlencoded());
+app.use(express.static('public'));
+
+app.post('/checkout', (req, res) => {
+  console.log(req.body);
+  res.send('ok')
+});
+
+// connection.query(insert...)
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+
+
+
+// Database setup
+
+// Connection
+var connection = mysql.createConnection({
+  user: 'root',
+  password: '',
+  database: 'formdata'
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+
+// hashing a password
+
+function makeSalt(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+function hashPassword(password) {
+  var salt = makeSalt(10);
+  var toHash = password + salt;
+  // using sha265
+  const hash = crypto.createHash('sha256');
+  return hash.update(toHash);
+}
